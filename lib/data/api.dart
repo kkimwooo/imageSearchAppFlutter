6a601053fed24b76b476c8b716d3f02c8796a151
kkimwooo <kkimwooo@gmail.com>
2022-01-02 22:08:25 +0900

    home_screen에서 api 호출 부분 분리
    - 비지니스 로직을 분리했지만 여전히 home_screen은
    Pixabay API에 의존적임.
    - 이것을 더 분리하는 작업이 필요하다.

---
+++ lib/data/api.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:flutter_clean_architecture/model/photo.dart';
+import 'package:http/http.dart' as http;
+
+class PixbayApi {
+  final baseUrl = 'https://pixabay.com/api/';
+  final key = '24999262-d78d2a61a8beebcf2664f2cde';
+
+  //http 통신 코드 비동기 처리 필요하기 때문에 Future
+  Future<List<Photo>> fetch(String query) async {
+    final response = await http.get(
+        Uri.parse('$baseUrl?key=$key&q=$query&image_type=photo&pretty=true'));
+    print('response.statusCode : ' + response.statusCode.toString());
+    //Map으로 가져와서 json 형태로 사용
+    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    //같은 형태가 반복되는 타입이라 Iterable 타입으로 표현
+    Iterable hits = jsonResponse['hits'];
+    return hits.map((e) => Photo.fromJson(e)).toList();
+  }
+}
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/data/api.dart';
 import 'package:flutter_clean_architecture/model/photo.dart';
 import 'package:flutter_clean_architecture/ui/widget/photo_widget.dart';
 import 'package:http/http.dart' as http;
@@ -11,22 +12,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final api = PixbayApi();
+
   final _controller = TextEditingController();
 
   List<Photo> _photos = [];
 
-  //http 통신 코드 비동기 처리 필요하기 때문에 Future
-  Future<List<Photo>> fetch(String query) async {
-    final response = await http.get(Uri.parse(
-        'https://pixabay.com/api/?key=24999262-d78d2a61a8beebcf2664f2cde&q=$query&image_type=photo&pretty=true'));
-    print('response.statusCode : ' + response.statusCode.toString());
-    //Map으로 가져와서 json 형태로 사용
-    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
-    //같은 형태가 반복되는 타입이라 Iterable 타입으로 표현
-    Iterable hits = jsonResponse['hits'];
-    return hits.map((e) => Photo.fromJson(e)).toList();
-  }
-
+  @override
   void dispose() {
     _controller.dispose();
     super.dispose();
@@ -59,12 +51,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 suffixIcon: IconButton(
                   //onPressed 안에서 사용할 함수가 async await 이기 때문에 여기서도 async await선언
                   onPressed: () async {
-                    final photos = await fetch(_controller.text);
+                    final photos = await api.fetch(_controller.text);
                     //State 변경이 일어나고 화면 새로 그려야 하므로 setState 사용
                     setState(() {
                       _photos = photos;
                     });
-                    print(_photos.toString());
                   },
                   icon: const Icon(Icons.search),
                 ),
